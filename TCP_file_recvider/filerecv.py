66cbd05635a37ce427fc292ae134610b862112ca
gaxzipTGO <kevin70309@gmail.com>
2022-11-16 17:30:55 +0800

    second-commit

--- TCP_file_recvider/filerecv.py
+++ TCP_file_recvider/filerecv.py
@@ -1,16 +1,25 @@
 import socket 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("127.0.0.1",5000))
-server.listen(5)
+import sys
 
-conn,addr = server.accept()
-file_len = int(conn.recv(1024).decode('utf-8'))
-print(file_len)
-conn.send(b'OK')
-file = b''
-while file_len > 0 :
-    file = file + conn.recv(1024)
-    file_len -= 1024
-    print(file_len)
+if __name__ == "__main__" :
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    if len(sys.argv) > 2 :
+        file_name = sys.argv[1]
+        IPAddress = input('please input your IP address')
+        server.bind((IPAddress,9500))
+        server.listen(5)
 
-print(file)
+        conn,addr = server.accept()
+        file_len = int(conn.recv(1024).decode('utf-8'))
+        print(file_len)
+        conn.send(b'OK')
+        file_byte = b''
+        while file_len > 0 :
+            file_byte = file_byte + conn.recv(1024)
+            file_len -= 1024
+            print(file_len)
+    file = open(file_name,'w',newline='')
+    file.write(file_byte.decode('utf-8'))
+    
+
+    
--- TCP_file_sender/filereader.py
+++ TCP_file_sender/filereader.py
@@ -1,43 +1,26 @@
 import json
 import socket
+import sys
 
 if __name__ == '__main__' :
-    file_path = input("please input file_input_path")
-    file_name = input("please input file_name")
-    file_data = input("please input file")
-    file = open(file_data,'r')
-    file_byte = file.read()
-    
-    data = {
-        "file_data" : file_byte,
-        "file_path" : file_path,
-        "file_name" : file_name
-    }
-    jsondata = json.dumps(data)
-    jsonfile = {
-        "file" :True,
-        "data" : data
-    }
-    
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(("169.254.138.194",9500))
-    send_byte = json.dumps(jsonfile).encode()
-    try :
-        client.send(str(len(send_byte)).encode())
-        ack = client.recv(1024)
-        if ack == b'OK' :
-            # print(json.dumps(file_byte).encode())
-            print(len(send_byte))
-            # while len(send_byte) > 0 :
-            client.sendall(send_byte)
-                # send_byte = send_byte[1024:]
-                # if client.recv(1024) == b'ACK' :
-                #     continue
-                # else :
-                #     raise OSError('send file not succelly')
-            print(client.recv(1024).decode('utf-8'))
-    except Exception as e :
-        print(str(e))
+
+    if len(sys.argv) > 2 :
+        file_data = sys.argv[1]
+        IPAdress = input("please input distination IP address")
+        file = open(file_data,'r')
+        file_byte = file.read()
+        
+        client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        client.connect((IPAdress,9500))
+        try :
+            client.send(str(len(file_byte)).encode())
+            ack = client.recv(1024)
+            if ack == b'OK' :
+                print(len(file_byte))
+                client.send(file_byte)
+                print(client.recv(1024).decode('utf-8'))
+        except Exception as e :
+            print(str(e))
 
 
     
